8fcb89d3c547826dac746bd6cb51118e601e1c75
Rodrigo Díaz <goediaz@gmail.com>
2019-09-25 17:34:06 +0200

    Update gameScene.ts
    Removing step3 from step2. Step2 should only be adding the physics to the object

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -22,17 +22,7 @@ class GameScene extends Phaser.Scene {
 
     create(): void {
         // set sky
-        this.add.image(400, 300, 'sky');
-
-        // set platforms
-        this.platforms = this.physics.add.staticGroup();
-        this.platforms
-            .create(400, 568, 'ground')
-            .setScale(2)
-            .refreshBody();
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');      
+        this.add.image(400, 300, 'sky');  
     }
 
     update(): void {
